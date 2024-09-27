cf2d69a12e7fb14510fb17ed2575a5fd95d00bd5
sherwin relampagos <sherwinrelampagos021@gmail.com>
2024-09-28 00:08:18 +0800

    Update feedback.php

--- feedback.php
+++ feedback.php
@@ -14,7 +14,10 @@ if (!isset($_SESSION["user"])) {
     <link rel="stylesheet" href="#.css">
 
     
-    <script src="loadLayout.js" defer></script>
+    <script src="loadLayout.js" defer>
+       
+    </script>
+    
 
     <title></title>
 </head>
@@ -22,11 +25,65 @@ if (!isset($_SESSION["user"])) {
 h1{text-align:center;
 
 }
+h2 {
+  background: #666;
+  font-family: sans-serif;
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+}
+
+form {
+  background: #fff;
+  position: relative;
+  display: inline-block;
+  padding: 20px;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%,-50%);
+  width: 500px;
+  border-radius: 5px;
+}
+
+
+form textarea {
+  width: calc(100% - 22px);
+  height: 100px;
+  resize: none;
+  border: 1px solid #999;
+  border-radius: 4px;
+  padding: 10px;
+}
+
+form button {
+  background: #999;
+  padding: 10px 20px;
+  color: #fff;
+  border: 1px solid #999;
+  border-radius: 4px;
+  transition: 0.1s;
+}
+
+form button:hover {
+  background: #fff;
+  color: #666;
+}
 </style>
 
 <body>
     <h1>feedback</h1>
+    <h2>
+        <form>
+  <br/>
+  <p>Please leave a comment below</p>
+  <textarea name="comment">"Comment Here"</textarea>
+  <br/>
+  <br/>
+  <button>Send Feedback</button>
+</form>
+</h2>
 </body>
+
 </html>
 
 
