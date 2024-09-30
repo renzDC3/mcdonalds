bf7f948b8ece10e9d6cdbd2b765768b82a1f72f8
renzDC3 <150541965+renzDC3@users.noreply.github.com>
2024-09-30 22:52:16 +0800

    Update account.php

--- account.php
+++ account.php
@@ -2,7 +2,11 @@
 session_start();
 if (!isset($_SESSION["user"])) {
    header("Location: login.php");
+   exit(); // Ensure that the script stops here
 }
+$crew_name = $_SESSION["crew_name"];
+$user = $_SESSION["user"];
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,41 +16,15 @@ if (!isset($_SESSION["user"])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="account.css">
-
-    
-    <script src="loadLayout.js" defer></script>
-
     <title>Account</title>
-    <style>
-        .logout-box {
-            display: flex;
-            justify-content: center; /* Centers the content horizontally */
-            align-items: center; /* Centers the content vertically */
-            height: 100vh; /* Takes the full height of the viewport */
-        }
-        
-        .logoutButton {
-            color: aliceblue;
-            background-color: black;
-            border: 2px white;
-            width: 220px;
-            height: 80px;
-            font-size: 45px;
-            text-align: center;
-            padding: auto;
-            cursor: pointer;
-            display: block;
-        }
-    </style>
 </head>
 <body>
-    <div class="logout-box">
-        <!-- Display User Info-->
-        <a href="logout.php" class="logoutButton btn btn-warning">Logout</a>
-    </div>
+    <div id="infoAccount">
+<h2><?php echo htmlspecialchars($crew_name); ?></h2>
+<h2><?php echo htmlspecialchars($user); ?></h2>
+</div>
+<div class="logoutbox">
+    <a href="login.php" class="logoutButton btn btn-warning" onclick="parent.location.href='logout.php'; return false;">Logout</a>
+</div>
 </body>
 </html>
-
-
-
-
