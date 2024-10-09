bde1d640a0b00d13757a420bfbba8825f0bfca20
renz dela cruz <renzdelacruz0za@gmail.com>
2024-10-09 14:23:09 +0800

    1

--- database.php
+++ database.php
@@ -3,12 +3,12 @@
 $hostName = "localhost";
 $dbUser = "root";
 $dbPassword = "";
-$dbName = "mcdonalds";
+$dbName = "mcdonalds_inventory";
 $conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
+ 
 ?>
 
 
